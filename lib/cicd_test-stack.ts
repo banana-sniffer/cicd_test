b1f7693ee07dce5c68b768a9a69adcb4890d5f78
William Tai <williamt@berkeley.edu>
2022-11-24 12:01:53 -0800

    Added two lambda functions

--- lib/cicd_test-stack.ts
+++ lib/cicd_test-stack.ts
@@ -3,19 +3,17 @@ import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage'
 
 export class CicdTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('banana-sniffer/cicd_test', 'main'), //Remember to change 
         commands: [
-          // 'pwd',
-          // 'ls',
-          // 'npm -v',
           'npm install',
           'npm ci',
           'npm run build', 
@@ -24,5 +22,19 @@ export class CicdTestStack extends Stack {
         primaryOutputDirectory: 'cdk.out',
       })
     });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "450729639506", region: "us-west-2" }
+    }));
+
+ 
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "450729639506", region: "us-west-2" }
+    }));
+
+
   }
 }
