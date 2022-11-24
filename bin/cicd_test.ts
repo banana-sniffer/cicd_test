c07b9f37489f83f7980ba75df4c2d523c12ecb44
William Tai <williamt@berkeley.edu>
2022-11-23 17:59:09 -0800

    first commit

---
+++ bin/cicd_test.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CicdTestStack } from '../lib/cicd_test-stack';
+
+const app = new cdk.App();
+new CicdTestStack(app, 'CicdTestStack', {
+  env: {
+    account: '450729639506',
+    region: 'us-west-1',
+  }
+});
+
+app.synth();
---
+++ lib/cicd_test-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('banana-sniffer/cicd_test', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
+  }
+}
