e88688495962ad8ddc5665f83ea2f9d7fb8d2ed5
William Tai <williamt@berkeley.edu>
2022-11-24 11:37:54 -0800

    is it the newest version

--- lib/cicd_test-stack.ts
+++ lib/cicd_test-stack.ts
@@ -1,10 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
+// import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
-export class CicdTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CicdTestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     new CodePipeline(this, 'Pipeline', {
@@ -12,10 +13,10 @@ export class CicdTestStack extends cdk.Stack {
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('banana-sniffer/cicd_test', 'main'), //Remember to change 
         commands: [
-          'pwd',
-          'ls',
-          'npm -v',
-          'npm install',
+          // 'pwd',
+          // 'ls',
+          // 'npm -v',
+          // 'npm install',
           'npm ci',
           'npm run build', 
           'npx cdk synth'
